b6bcf73b1176aeebe9a763bb42ccafc4ef131722
Thuy Nguyen <ttruong06@gmail.com>
2022-07-14 19:00:57 -0400

    set up server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 const path = require('path');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
