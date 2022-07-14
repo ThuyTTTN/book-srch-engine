68aa9bea7e11c158ca076eadd1a0c57afacbe3cd
Thuy Nguyen <ttruong06@gmail.com>
2022-07-14 19:46:10 -0400

    working on resolvers.js file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -6,15 +8,11 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
-const path = require('path');
-const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
-const routes = require('./routes');
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -27,6 +25,9 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const routes = require('./routes');
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
