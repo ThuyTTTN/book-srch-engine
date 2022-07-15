025e05116a9691d7537a563a1a3253c12ca3915b
Thuy Nguyen <ttruong06@gmail.com>
2022-07-15 14:16:00 -0400

    debugged resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,63 +6,66 @@ const resolvers = {
     Query: {
       me: async (parent, args, context) => {
         if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id })
-                .select('-__v -password')
-            return userData;
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            // .populate('savedBooks')
+
+          return userData;
         }
+
         throw new AuthenticationError('Not logged in');
+      }
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+      
+        return { token, user };
       },
-      Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-          
-            return { token, user };
-          },
-          login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-          
-            if (!user) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-          
-            const correctPw = await user.isCorrectPassword(password);
-          
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-          
-            const token = signToken(user);
-            return { token, user };
-          },
-          saveBook: async (parent, { bookBody }, context) => {
-            if (context.user) {
-                const updatedUser = await User.findOneAndUpdate(
-                  { _id: context.user._id },
-                  { $push: { savedBooks: bookBody } },
-                  { new: true, runValidators: true }
-                )
-            
-                return updatedUser;
-              }
-            
-              throw new AuthenticationError('You need to be logged in!');
-          },
-          removeBook: async (parent, { bookId }, context) => {
-            if (context.user) {
-                const updatedUser = await User.findOneAndUpdate(
-                  { _id: context.user._id },
-                  { $pull: { savedBooks: bookId } },
-                  { new: true }
-                );
-            
-                return updatedUser;
-              }
-            
-              throw new AuthenticationError('You need to be logged in!');
-          }
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const token = signToken(user);
+        return { token, user };
+      },
+      saveBook: async (parent, { bookBody }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $push: { savedBooks: bookBody } },
+              { new: true }
+          );
+
+          return updatedUser;
+        }
+
+        throw new AuthenticationError('You need to be logged in!');
+      },
+      removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {      
+          const updatedUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: {bookId} } },
+              { new: true }
+          );
+      
+          return updatedUser;
+        }
+    
+        throw new AuthenticationError('You need to be logged in!');
       }
     }
-  };
+};
   
   module.exports = resolvers;
