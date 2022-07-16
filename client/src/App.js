95689b54e2d08bd0fd1ec1e0f65889388e687aea
Thuy Nguyen <ttruong06@gmail.com>
2022-07-16 09:25:28 -0400

    changed import link

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider, ApolloClient } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -4,7 +4,7 @@ import { useMutation, useQuery } from '@apollo/react-hooks';
 
 
 // import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
+//import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { REMOVE_BOOK } from '../utils/mutations';
 import { GET_ME } from '../utils/queries';
